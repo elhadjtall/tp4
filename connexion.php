d427506a715f95e435f90ee764e1152fd9ff1989
Your Name <you@example.com>
2023-02-27 15:38:02 +0100

    Formulaire de connexion

---
+++ connexion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connexion</h1>
+    <form action="" medhot="POST">
+        <input type="email"     name="email"     placeholder="email"><br><br>
+        <input type="password"  name="password"  placeholder="password"><br><br>
+        <input type="submit"    value="connexion"> 
+
+    </form>
+</body>
+</html>
