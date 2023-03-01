15870bfb16a85b6a29cec9167bbfb44c57cb0bfb
Your Name <you@example.com>
2023-03-01 13:32:04 +0100

    stye css + modif

--- connexion.php
+++ connexion.php
@@ -5,19 +5,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Connexion</title>
 </head>
 <body>
-<div class="logo">
-    <img src="ymoney.png" alt="">
-</div>
+    <header class="main-head">
+        <nav>
+        <h1 id="logo">YMONEY</h1>
+        </nav>
+        
+    </header>
     <form action="" medhot="POST">
-    <h1>Connexion</h1>
+    <h1 id="con">Connexion</h1>
        <p><LAbel for="">VOTRE ADRESSE E-MAIL</LAbel></p>
         <p><input type="email"     name="email"         placeholder="email"></p>
         <p><label for="">VOTRE MOT DE PASSE</label></p>
         <p><input type="password"  name="password"      placeholder="password"></p>
+        <p><label for="mtp">J'ai oublié mon mot de passe</label></p>
         <p><input type="submit"    name="submit"        value="CONTINUER"></p>
+        <p><label for="creer">JE VEUX CREER UN COMPTE</label></p>
     </form>
 </body>
 </html>
